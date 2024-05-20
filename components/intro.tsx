33b6849da168dba07c9cfe368f31f221c19a92e4
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2024-05-20 23:55:00 +0100

    Update profile image

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,11 +34,11 @@ export default function Intro() {
             <Image
               src={profileImg}
               alt="Jake portrait"
-              width="192"
-              height="192"
+              width="256"
+              height="256"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-32 w-32 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
