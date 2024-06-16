4fb37e71060cf7afb9fb15a2e765b06419090d3a
Jake Ryu <jake.ryu@outlook.com>
2024-06-16 10:32:05 +0100

    Update about me

--- components/about.tsx
+++ components/about.tsx
@@ -25,45 +25,43 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         My name is Jake Ryu. I was born and raised in South Korea, where I began
-        my journey as a software developer. It&apos;s been{" "}
-        {`${calculateYears(2000)}`} years since I started. After gaining six
-        years of experience in Korea, I moved to the UK to help build the local
-        systems for Hyundai Motor Company. I have since settled here and am
-        currently working as a Lead Developer at{" "}
-        <a href="https://www.barnett-waddingham.co.uk/" target="_blank">
+        my journey as a software developer. With {`${calculateYears(2000)}`}{" "}
+        years of experience in the field, I have honed my skills and expertise
+        across various technologies and projects. After gaining six years of
+        experience in Korea, I moved to the UK to help build the local systems
+        for{" "}
+        <a
+          className="italic font-bold"
+          href="https://www.hyundai.com/uk/en.html"
+          target="_blank"
+        >
+          Hyundai Motor Company
+        </a>
+        . I have since settled here and am currently working as a Lead Developer
+        at{" "}
+        <a
+          className="italic font-bold"
+          href="https://www.barnett-waddingham.co.uk/"
+          target="_blank"
+        >
           Barnett Waddingham
         </a>
-        , a financial services company specializing in workplace pensions. I
-        primarily work with <span className="italic font-medium">.NET</span> and{" "}
-        <span className="italic font-medium">React</span> to create amazing
-        projects.
+        , a financial services company specializing in workplace pensions.
       </p>
 
       <p className="mb-3">
-        I have a keen interest in web and solution architecture. When designing
-        backends, a solid architecture is crucial for providing stable services.
-        The world of software presented through the browser also fascinates me.
-        While the web has made tremendous strides in the past, it continues to
-        evolve rapidly. For instance, the choice of Next.js to overcome some of
-        React’s limitations is a testament to the fast pace of web development.
-        In this ever-growing field, I strive to stay current with the latest
-        technology trends and best practices.
-      </p>
-
-      <p className="mb-3">
-        I believe that developers have a responsibility to implement
-        requirements accurately and efficiently. We all know the importance of
-        tests in enhancing code quality and minimizing errors. This is why I
-        frequently utilise methodologies like{" "}
+        I believe in the responsibility of developers to implement requirements
+        accurately and efficiently. Quality assurance through testing is
+        paramount in my development process. Utilising methodologies like{" "}
         <span className="italic font-medium">Clean Architecture</span> and{" "}
-        <span className="italic font-medium">Domain-Driven Design (DDD)</span>.
-        Clean Architecture simplifies testing by managing dependencies in a
-        single direction, while DDD enhances the verification of business logic
-        through clear domain models. These approaches help me write robust and
-        maintainable code.
+        <span className="italic font-medium">Domain-Driven Design (DDD)</span>,
+        I manage dependencies effectively and verify business logic through
+        clear domain models. These approaches enable me to write robust,
+        maintainable, and scalable code.
       </p>
+
       <p className="mb-3">
-        When I’m not coding, I enjoy watching English Premier League (EPL)
+        When I&apos;m not coding, I enjoy watching English Premier League (EPL)
         matches. I also love reading novels and self-improvement books.
       </p>
     </motion.section>
